073c7e1b0803118bbb89e76a4d7e01c69f4dd7fc
DylanAG23 <dylan.avellaneda23@gmail.com>
2025-04-23 12:04:28 -0500

    parcial primero

---
+++ baseDatos.js
@@ -0,0 +1,16 @@
+const { Client } = require('pg');
+
+const client = new Client({
+  host: 'aws-0-us-east-1.pooler.supabase.com',
+  port: 5432,
+  user: 'postgres.qhbesaltdklufrpdxglk',
+  password: 'lGME80s6ImCZviZc ',
+  database: 'parcialBD',
+  ssl: { rejectUnauthorized: false } // necesario para Supabase
+});
+
+client.connect()
+  .then(() => console.log('Conectado a Supabase'))
+  .catch(err => console.error('Error en la conexión:', err.message));
+
+module.exports = client;
